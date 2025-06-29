9bcb46a773bd9f21893204168ea4179a419e2c70
niveyrojulian <niveyrojulian5@gmail.com>
2025-06-29 20:41:20 -0300

    fix: cors

--- client/src/redux/actions/rutaBack.js
+++ client/src/redux/actions/rutaBack.js
@@ -1,3 +1,3 @@
-// const rutaBack = "https://server-espigadeoro.vercel.app";
-const rutaBack = "http://localhost:3001";
+const rutaBack = "https://server-espigadeoro.vercel.app";
+// const rutaBack = "http://localhost:3001";
 export default rutaBack;

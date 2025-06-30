2760eabc60f7617721e74ea3ce6fea6ea8c695a9
niveyrojulian <niveyrojulian5@gmail.com>
2025-06-30 11:15:03 -0300

    fix: vercel

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('../generated/prisma/client')
 
 // Evitar múltiples instancias de PrismaClient en desarrollo
 const globalForPrisma = global
